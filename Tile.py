6112bdc8bcfc8d0b61198a1d81553509ff3c7939
VijayAnand <88843749+vijayanand1212@users.noreply.github.com>
2022-05-09 22:10:12 +0530

    Update Tile.py

--- Tile.py
+++ Tile.py
@@ -5,17 +5,3 @@ class Tile:
         self.pos = pos
         self.slicedImageSize = slicedImageSize
         self.tileNumber = tileNumber
-    def change_pos(self,newpos,move):
-        i,j = newpos
-        x = j * self.IMAGE_HEIGHT + (j * 5) + 10
-        y = i * self.IMAGE_WIDTH + (i * 5) + 10
-        if move == 'up':
-            self.rectangle[x][y].y -= self.IMAGE_HEIGHT + 5
-        if move == 'down':
-            self.rectangle[x][y].y += self.IMAGE_HEIGHT + 5
-        if move == 'left':
-            self.rectangle[x][y].y -= self.IMAGE_WIDTH + 5
-        if move == 'right':
-            self.rectangle[x][y].y += self.IMAGE_WIDTH + 5
-        self.pos = newpos
-
